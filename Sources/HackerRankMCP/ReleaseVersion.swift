26aab00ffd5ba466f829f59fde14ecc4158dfd39
Adam Dangoor <adamdangoor@gmail.com>
2026-09-08 13:48:16 +0100

    Prepare 0.2.0 release (#98)

--- Sources/HackerRankMCP/ReleaseVersion.swift
+++ Sources/HackerRankMCP/ReleaseVersion.swift
@@ -1,6 +1,6 @@
 /// Single source of truth for the semver advertised by the standalone server.
 public enum HackerRankMCPVersion {
-    public static let semver = "0.1.1"
+    public static let semver = "0.2.0"
 }
 
 /// The semantic version advertised by the standalone server.
